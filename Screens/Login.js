6bd53d63dab06f1fa519757c693abdd22d4b5996
TyereeTinker <tyereetinker31@gmail.com>
2024-01-06 23:47:35 -0500

    Login design

--- Screens/Login.js
+++ Screens/Login.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, Button, TextInput, Image, SafeAreaView, Touchab
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../config/firebase";
 
-const backImg = require("../assets/back.jpg")
+const backImg = require("../assets/back2.jpg")
 
 export default function Login({ navigation }) {
 
@@ -20,7 +20,32 @@ export default function Login({ navigation }) {
 
     return (
         <View style={style.container}>
-            <Text>Login</Text>
+            <Image source={backImg} style={style.backImage} />
+            <View style={style.whiteSheet}>
+                <SafeAreaView style={style.form}>
+                    <Text style={style.title}>Log In</Text>
+                    <TextInput
+                        style={style.input}
+                        placeholder="Enter email"
+                        autoCapitalize="none"
+                        keyboardType="email-address"
+                        textContentType="emailAddress"
+                        autoFocus={true}
+                        value={email}
+                        onChangeText={(text) => setEmail(text)}
+                    />
+                    <TextInput
+                        style={style.input}
+                        placeholder="Enter password"
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        secureTextEntry={true}
+                        textContentType="password"
+                        value={password}
+                        onChangeText={(text) => setPassword(text)}
+                    />
+                </SafeAreaView>
+            </View>
         </View>
     )
 }
@@ -29,5 +54,41 @@ const style = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#0000FF"
-    }
+    },
+    whiteSheet: {
+        width: '100%',
+        height: '75%',
+        position: "absolute",
+        marginTop: '10%',
+        bottom: 0,
+        backgroundColor: '#fff',
+        borderTopRightRadius: 60,
+    },
+    backImage: {
+        width: "100%",
+        height: 340,
+        position: "absolute",
+        top: 0,
+        resizeMode: 'cover',
+    },
+    title: {
+        fontSize: 36,
+        fontWeight: 'bold',
+        color: "black",
+        alignSelf: "center",
+        paddingBottom: 24,
+    },
+    form: {
+        flex: 1,
+
+        marginHorizontal: 30,
+    },
+    input: {
+        backgroundColor: "#F6F7FB",
+        height: 58,
+        marginBottom: 20,
+        fontSize: 16,
+        borderRadius: 10,
+        padding: 12,
+    },
 })
